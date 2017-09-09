72876d63ef324a7dd564918b7cdac8de47f33c3c
Jared Patrick <japatric@cisco.com>
2017-09-08 20:24:16 -0400

    added methods to queue and tests

--- queue/queue.go
+++ queue/queue.go
@@ -4,39 +4,90 @@ import (
 	"concord-pq/task"
 )
 
-// PriorityQueue is a min binary heap implementation of a priority queue data structure
+// PriorityQueue is a min binary heap implementation of a priority queue data
+// structure.
 type PriorityQueue struct {
-	Count int
+	// Key is the task resource key
+	// count is the number of task nodes in the heap
+	// heap is the binary heap where task nodes are stored
 	Key   string
-	nodes []*task.Task
+	count int
+	heap  []*task.Task
 }
 
-// Insert inserts a task into the task nodes in priority order
-func (pq *PriorityQueue) Insert(t *task.Task) {
-	pq.nodes = append(pq.nodes, t)
-	i := len(pq.nodes) - 1
+// NewPriorityQueue returns an initialized priority queue instance.
+func NewPriorityQueue(key string) *PriorityQueue {
+	return &PriorityQueue{key, 0, make([]*task.Task, 0)}
+}
+
+// List returns all priority queue nodes.
+func (pq *PriorityQueue) List() []*task.Task {
+	return pq.heap
+}
+
+// Peek returns the min heap node without modifying the heap.
+func (pq *PriorityQueue) Peek() *task.Task {
+	return pq.heap[0]
+}
+
+// Pop removes and returns the min heap node.
+func (pq *PriorityQueue) Pop() *task.Task {
+	if pq.count == 0 {
+		return nil
+	}
+	min := pq.heap[0]
+	pq.heap[0] = pq.heap[pq.count-1]
+	pq.heap = pq.heap[:pq.count-1]
+	pq.minHeapify(0)
+	pq.count--
+
+	return min
+}
+
+// Push inserts a task into the task nodes in priority order.
+func (pq *PriorityQueue) Push(t *task.Task) {
+	pq.heap = append(pq.heap, t)
+	i := len(pq.heap) - 1
 
 	for i > 0 {
 		parent := (i - 1) / 2
 
-		if pq.nodes[i].Priority < pq.nodes[parent].Priority {
-			pq.nodes[i] = pq.nodes[parent]
-			pq.nodes[parent] = t
+		if pq.heap[i].Priority < pq.heap[parent].Priority {
+			pq.heap[i] = pq.heap[parent]
+			pq.heap[parent] = t
 			i = parent
 		} else {
 			break
 		}
 	}
 
-	pq.Count++
+	pq.count++
 }
 
-// List returns all priority queue nodes
-func (pq *PriorityQueue) List() []*task.Task {
-	return pq.nodes
+// minHeapify the MaxHeapify function on the priority queue nodes.
+func (pq *PriorityQueue) minHeapify(i int) {
+	MinHeapify(pq.heap, i)
 }
 
-// NewPriorityQueue returns an initialized priority queue instance
-func NewPriorityQueue(key string) *PriorityQueue {
-	return &PriorityQueue{Count: 0, Key: key}
+// MinHeapify places the target parent node in the proper position in
+// the binary heap.
+//
+// MinHeapify assumes all subtrees are proper binary heaps.
+func MinHeapify(nodes []*task.Task, i int) {
+	left := (i * 2) + 1
+	right := (i * 2) + 2
+	min := i
+
+	if left < len(nodes) && nodes[left].Priority < nodes[i].Priority {
+		min = left
+	}
+	if right < len(nodes) && nodes[right].Priority < nodes[i].Priority {
+		min = right
+	}
+	if min != i {
+		node := nodes[i]
+		nodes[i] = nodes[min]
+		nodes[min] = node
+		MinHeapify(nodes, min)
+	}
 }
--- queue/queue_test.go
+++ queue/queue_test.go
@@ -12,12 +12,23 @@ func TestNewPriorityQueue(t *testing.T) {
 	if pq.Key != "key" {
 		t.Fatal("expected key to be 'key'")
 	}
-	if pq.Count != 0 {
-		t.Fatal("expected node count to be 0")
+}
+
+func TestPriorityQueuePeek(t *testing.T) {
+	tasks := []*task.Task{
+		&task.Task{Priority: 13.5},
+		&task.Task{Priority: 11.5},
+	}
+	pq := queue.NewPriorityQueue("key")
+	for _, task := range tasks {
+		pq.Push(task)
+	}
+	if pq.Peek() != tasks[1] {
+		t.Fatal("peek returned an unexpected task")
 	}
 }
 
-func TestPriorityQueueInsert(t *testing.T) {
+func TestPriorityQueuePush(t *testing.T) {
 	tasks := []*task.Task{
 		&task.Task{Priority: 22.5},
 		&task.Task{Priority: 3.5},
@@ -25,13 +36,58 @@ func TestPriorityQueueInsert(t *testing.T) {
 	}
 	pq := queue.NewPriorityQueue("key")
 	for _, task := range tasks {
-		pq.Insert(task)
+		pq.Push(task)
 	}
-	if pq.Count != 3 {
-		t.Fatal("expected node count to be 3")
+	heap := [3]float64{}
+	for i, task := range pq.List() {
+		heap[i] = task.Priority
 	}
-	nodes := pq.List()
-	if nodes[0].Priority != 3.5 || nodes[1].Priority != 22.5 || nodes[2].Priority != 16.5 {
+	if heap != [3]float64{3.5, 22.5, 16.5} {
 		t.Fatal("invalid node order")
+	}
+}
+
+func TestMinHeapify(t *testing.T) {
+	heap := [7]float64{}
+	tasks := []*task.Task{
+		&task.Task{Priority: 75.5},
+		&task.Task{Priority: 63.5},
+		&task.Task{Priority: 22.5},
+		&task.Task{Priority: 72.5},
+		&task.Task{Priority: 65.5},
+		&task.Task{Priority: 55.5},
+		&task.Task{Priority: 80.5},
+	}
+	queue.MinHeapify(tasks, 0)
+	for i, task := range tasks {
+		heap[i] = task.Priority
+	}
+	if heap != [7]float64{22.5, 63.5, 55.5, 72.5, 65.5, 75.5, 80.5} {
+		t.Fatal("invalid node order")
+	}
+}
+
+func TestPriorityQueuePop(t *testing.T) {
+	tasks := []*task.Task{
+		&task.Task{Priority: 22.5},
+		&task.Task{Priority: 5.5},
+		&task.Task{Priority: 125.5},
+	}
+	pq := queue.NewPriorityQueue("key")
+	for _, task := range tasks {
+		pq.Push(task)
+	}
+
+	if pq.Pop().Priority != tasks[1].Priority {
+		t.Fatal("pop returned an unexpected task")
+	}
+	if pq.Pop().Priority != tasks[0].Priority {
+		t.Fatal("pop returned an unexpected task")
+	}
+	if pq.Pop().Priority != tasks[2].Priority {
+		t.Fatal("pop returned an unexpected task")
+	}
+	if pq.Pop() != nil {
+		t.Fatal("expected pop to return nil")
 	}
 }
